f845f54d61958c0bf2e39d7f0d9c875a4b4afdbd
Lee  Albert Raymond <albert.rlee@hotmail.ca>
2017-11-08 11:18:37 -0500

    Begin work on tomasulo.c. fetch/fetch->dispatch complete.

--- tomasulo.c
+++ tomasulo.c
@@ -99,6 +99,9 @@ static instruction_t* map_table[MD_TOTAL_REGS];
 //the index of the last instruction fetched
 static int fetch_index = 0;
 
+static int fuINTFill = 0;
+static int fuFPFill = 0;
+
 /* FUNCTIONAL UNITS */
 
 
@@ -132,7 +135,7 @@ static bool is_simulation_done(counter_t sim_insn) {
 void CDB_To_retire(int current_cycle) {
 
   /* ECE552: YOUR CODE GOES HERE */
-
+     
 }
 
 
@@ -147,6 +150,10 @@ void CDB_To_retire(int current_cycle) {
 void execute_To_CDB(int current_cycle) {
 
   /* ECE552: YOUR CODE GOES HERE */
+    
+    // stores, conditional/unconditional branches, jumps & calls do not write to CDB
+    // no forwarding/bypassing support. values broadcasted by the CDB can be read in the next cycle
+    // if 2+ instructions compete for a resource (FU, CDB), priority to older instruction (done in issue_To_execute)
 
 }
 
@@ -163,6 +170,12 @@ void execute_To_CDB(int current_cycle) {
 void issue_To_execute(int current_cycle) {
 
   /* ECE552: YOUR CODE GOES HERE */
+    
+    // prioritize old instructions over new instructions from dispatch (if more than 1 ready to execute)
+    // execute in the functional unit which matches its reservation station
+    // ignore dependences over memory accesses (loads/stores do not have to wait for each oher if they access the same address)
+    // remain in reservation station until execute completed
+    // multiple instructions can enter the execute stage in the same cycle
 }
 
 /* 
@@ -176,6 +189,60 @@ void issue_To_execute(int current_cycle) {
 void dispatch_To_issue(int current_cycle) {
 
   /* ECE552: YOUR CODE GOES HERE */
+      
+    // check if a reservation station is available
+    // if not, stall
+
+    // instruction requires a floating reservation station
+    if (IS_FCOMP(instr_queue[0]->op))
+    {
+        // check that a floating reservation station is available
+        if (fuFPFill == 4)
+            // no floating reservation station available. stall
+            return;
+        else
+        {
+            // reservation station available for our fp instruction
+            // start adding the instruction and its information into the reservation station
+            reservFP[fuFPFill] = instr_queue[0];
+            
+            fuFPFill++;
+        }
+    }
+
+    // instruction requires an integer reservation station
+    if (IS_ICOMP(instr_queue[0]->op))
+    {
+        // check that an integer reservation station is available
+        if (fuINTFill == 4)
+            // no integer reservation station available. stall
+            return;
+        else
+        {
+            // reservation station available for our int instruction
+            // start adding the instruction and its information into the reservation station
+            reservINT[fuINTFill] = instr_queue[0];
+            
+            fuINTFill++;
+        }
+    }
+    
+    // instruction does not require a reservation station. send to issue immediately
+    if (IS_UNCOND_CTRL(instr_queue[0]->op) || IS_COND_CTRL(instr_queue[0]->op))
+    {
+        instr_queue[0]->tom_issue_cycle = current_cycle;
+        return;
+    }
+    
+    // allocate a reservation station entry based on the instructions opcode
+    // - do not issue a reservation station for an unconditional branch
+    // - handle conditional branches the same way as unconditional branches
+    // - load/stores use integer FU and their respective reservation stations
+    // mark any RAW dependencies in the reservation entry
+    
+    // if there is RAW hazard, stall
+    // if there are no FU's, stall
+    
 }
 
 /* 
@@ -188,7 +255,52 @@ void dispatch_To_issue(int current_cycle) {
  */
 void fetch(instruction_trace_t* trace) {
 
-  /* ECE552: YOUR CODE GOES HERE */
+    /* ECE552 Assignment 3 - BEGIN CODE */
+    
+    // attempt to grab an instruction from the instruction trace
+    // if the instruction is a trap instruction, skip (do not insert into IFQ)
+    // otherwise, insert into IFQ in program-order (FIFO essentially)
+    
+    // first check that there's space in the IFQ
+    if (instr_queue_size < INSTR_QUEUE_SIZE)
+    {
+        // trace starts at instruction 1, not instruction 0
+        // therefore increment fetch_index before grabbing instruction
+        fetch_index++;
+        
+        // grab the next instruction in the trace
+        instruction_t *inst = get_instr(trace, fetch_index);
+        
+        // if the inst is empty, trace is complete
+        if (inst == NULL)
+            return;
+        // if the inst is a trap inst, rerun fetch to grab the next inst
+        else if (IS_TRAP(inst->op))
+        {
+            fetch(trace);
+            return;
+        }
+        else
+        {
+            // initialize values in the instruction_t data structure
+            inst->Q[0] = NULL;
+            inst->Q[1] = NULL;
+            inst->Q[2] = NULL;
+            
+            inst->tom_cdb_cycle = 0;
+            inst->tom_dispatch_cycle = 0;
+            inst->tom_execute_cycle = 0;
+            inst->tom_issue_cycle = 0;
+            
+            // add instruction to IFQ
+            // fetch completed at this point
+            instr_queue[instr_queue_size] = inst;
+            instr_queue_size++;
+            return;
+        }
+    }
+    
+    /* ECE552 Assignment 3 - END CODE */
 }
 
 /* 
@@ -205,6 +317,16 @@ void fetch_To_dispatch(instruction_trace_t* trace, int current_cycle) {
   fetch(trace);
 
   /* ECE552: YOUR CODE GOES HERE */
+  // check if there is an instruction in the IFQ
+  if (instr_queue_size == 0)
+      return;
+  
+  // send instructions to dispatch after being fetched
+  for (int i = 0; i < instr_queue_size; i++)
+  {
+      if (instr_queue[i]->tom_dispatch_cycle == 0)
+          instr_queue[i]->tom_dispatch_cycle = current_cycle;
+  }
 }
 
 /* 
